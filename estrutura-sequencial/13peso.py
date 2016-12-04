7bc0f59dcf930df01db1dcd0c109ceb0a5bd0b47
Felipe Oliveira <fsoliveira@live.com>
2016-12-04 01:20:29 -0300

    Add files via upload

--- estrutura-sequencial/13peso.py
+++ estrutura-sequencial/13peso.py
@@ -6,40 +6,42 @@ Para mulheres: (62.1*h) - 44.7 (h = altura)
 Peça o peso da pessoa e informe se ela está dentro, acima ou abaixo do peso.
 
 """
+def main():
 
-sexo = input("Informe seu sexo: (1) para masculino (2) para feminino:")
+    sexo = input("Informe seu sexo: (1) para masculino (2) para feminino:")
 
-if sexo == str(1):
+    if sexo == str(1):
 
-    aM = float(input("Informe sua altura em metros:"))
-    peso_real = float(input("Informe seu peso atual:"))
-    peso_ideal = float((78.7 * aM) - 58)
+        aM = float(input("Informe sua altura em metros:"))
+        peso_real = float(input("Informe seu peso atual:"))
+        peso_ideal = float((78.7 * aM) - 58)
 
-    if peso_real > peso_ideal:
-        print("O peso informado é: %2.fKg. Você está acima do peso. Procure estar o mais próximo de: %2.fKg" %(
-        peso_real, peso_ideal))
-
-    elif peso_real < peso_ideal:
-        print("O peso informado é: %2.fKg,. Você está abaixo do peso. Procure estar o mais próximo de: %2.fKg" %(
-        peso_real, peso_ideal))
-
-    elif peso_real == peso_ideal:
-        print("O peso informado é: %2.fKg,. Você está em forma" % peso_ideal)
-
-elif sexo == str(2):
-
-    aF = float(input("Informe sua altura em metros:"))
-    peso_real = float(input("Informe seu peso atual:"))
-    peso_ideal = float((62.1 * aF) - 44.7)
-
-    if peso_real > peso_ideal:
-        print("O peso informado é: %2.fKg. Você está acima do peso. Procure estar o mais próximo de: %2.fKg" % (
+        if peso_real > peso_ideal:
+            print("O peso informado é: %2.fKg. Você está acima do peso. Procure estar o mais próximo de: %2.fKg" %(
             peso_real, peso_ideal))
 
-    elif peso_real < peso_ideal:
-        print("O peso informado é: %2.fKg,. Você está abaixo do peso. Procure estar o mais próximo de: %2.fKg" % (
+        elif peso_real < peso_ideal:
+            print("O peso informado é: %2.fKg,. Você está abaixo do peso. Procure estar o mais próximo de: %2.fKg" %(
             peso_real, peso_ideal))
 
-    elif peso_real == peso_ideal:
-        print("O peso informado é: %2.fKg,. Você está em forma" % peso_ideal)
+        elif peso_real == peso_ideal:
+            print("O peso informado é: %2.fKg,. Você está em forma" % peso_ideal)
 
+    elif sexo == str(2):
+
+        aF = float(input("Informe sua altura em metros:"))
+        peso_real = float(input("Informe seu peso atual:"))
+        peso_ideal = float((62.1 * aF) - 44.7)
+
+        if peso_real > peso_ideal:
+            print("O peso informado é: %2.fKg. Você está acima do peso. Procure estar o mais próximo de: %2.fKg" % (
+                peso_real, peso_ideal))
+
+        elif peso_real < peso_ideal:
+            print("O peso informado é: %2.fKg,. Você está abaixo do peso. Procure estar o mais próximo de: %2.fKg" % (
+                peso_real, peso_ideal))
+
+        elif peso_real == peso_ideal:
+            print("O peso informado é: %2.fKg,. Você está em forma" % peso_ideal)
+
+main()
